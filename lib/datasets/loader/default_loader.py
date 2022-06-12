cd65cd9c659a534ab774eb10be15aa3bd83ad372
pauls3 <paulstanik3@gmail.com>
2022-06-12 02:22:10 -0700

    printing out different part of code

--- lib/datasets/loader/default_loader.py
+++ lib/datasets/loader/default_loader.py
@@ -286,8 +286,8 @@ class CSDataTestLoader(data.Dataset):
     def __list_dirs(self, root_dir, dataset):
         img_list = list()
         name_list = list()
+        print(root_dir, dataset)
         image_dir = os.path.join(root_dir, dataset)
-        print(image_dir)
         img_extension = os.listdir(image_dir)[0].split('.')[-1]
 
         if self.configer.get('dataset') == 'cityscapes':
